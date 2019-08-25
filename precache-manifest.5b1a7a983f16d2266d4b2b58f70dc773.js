1ef6447738fe61415ea436d08c169c010b0f2e6a
Mark11101 <r-io-home@ya.ru>
2019-08-25 23:40:31 +0400

    Updates

---
+++ precache-manifest.5b1a7a983f16d2266d4b2b58f70dc773.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b334ff6bf56b5501d0a7468062eab86",
+    "url": "/MultySelect/index.html"
+  },
+  {
+    "revision": "3ec4e2f96350263974f9",
+    "url": "/MultySelect/static/css/main.e9c34483.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/MultySelect/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "3ec4e2f96350263974f9",
+    "url": "/MultySelect/static/js/main.0388679e.chunk.js"
+  },
+  {
+    "revision": "ed103aa9b53eeeb66fdc",
+    "url": "/MultySelect/static/js/runtime~main.5d973689.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MultySelect/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/multyselectpage/precache-manifest.dbdca4ed36c921698730912581853ff8.js"
+  "/MultySelect/precache-manifest.5b1a7a983f16d2266d4b2b58f70dc773.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/multyselectpage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MultySelect/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
